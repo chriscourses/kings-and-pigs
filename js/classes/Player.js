f7920dc9cb9ed38cb484f317e4fae801e9a37521
Christopher Lis <christopher4lis@gmail.com>
2022-09-12 18:01:20 -0400

    Gravity

--- js/classes/Player.js
+++ js/classes/Player.js
@@ -5,11 +5,17 @@ class Player {
       y: 100,
     }
 
+    this.velocity = {
+      x: 0,
+      y: 0,
+    }
+
     this.width = 100
     this.height = 100
     this.sides = {
       bottom: this.position.y + this.height,
     }
+    this.gravity = 1
   }
 
   draw() {
@@ -18,9 +24,12 @@ class Player {
   }
 
   update() {
-    if (this.sides.bottom < canvas.height) {
-      this.position.y++
+    this.position.y += this.velocity.y
+
+    // above bottom of canvas
+    if (this.sides.bottom + this.velocity.y < canvas.height) {
+      this.velocity.y += this.gravity
       this.sides.bottom = this.position.y + this.height
-    }
+    } else this.velocity.y = 0
   }
 }
